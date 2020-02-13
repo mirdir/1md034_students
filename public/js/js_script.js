38b83ce9a3dba1ae086371a8b7e88a5828e6a258
Miranda Kiss-Deáki <miranda.kissdeaki@gmail.com>
2020-02-13 21:05:24 +0100

    vue grid boxes too

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -27,7 +27,7 @@ let randomBurger1 = new menuItem("Random burger 1", "d1", "d2", "d3", false, "10
 
 let randomBurger2 = new menuItem("Random burger 2", "d1", "d2", "d3", false, "1000", "https://www.pinclipart.com/picdir/big/13-132538_burger-svg-png-icon-free-download-545640-clip.png");
 //document.getElementById("b5").innerHTML = getNameKcal(randomBurger2);
-
+/*
 menu = [soph, greedy, dirty, randomBurger1, randomBurger2];
 
 let burgerElem = document.getElementsByClassName("burger");
@@ -79,6 +79,5 @@ for (let i = 0; i < burgerElem.length; i++) {
   desc3.appendChild(d3);
   gluten.appendChild(glut);
   calories.appendChild(cal);
-
-
 }
+*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,13 +1,8 @@
-/*
+
 const vm = new Vue({
-  el: '#burgers',
+  el: '#wrapperID',
   data: {
     menu: [soph, greedy, dirty, randomBurger1, randomBurger2]
-    burger1: getNameKcal(soph),
-    burger2: getNameKcal(greedy),
-    burger3: getNameKcal(dirty),
-    burger4: getNameKcal(randomBurger1),
-    burger5: getNameKcal(randomBurger2)
+
   }
 })
-*/
