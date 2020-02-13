92adecf6c86ea52b5ff4eb425351fec2433dea14
Miranda Kiss-Deáki <miranda.kissdeaki@gmail.com>
2020-02-13 20:41:11 +0100

    done js part of grid boxes

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,36 +1,84 @@
-function menuItem(burger, algr, cal, path) {
-  this.burger = burger;
-  this.allergies = algr;
+function menuItem(name, d1, d2, d3, gluten, cal, path) {
+  this.name = name;
+  this.d1 = d1;
+  this.d2 = d2;
+  this.d3 = d3;
+  this.gluten = gluten;
   this.calories = cal;
   this.path = path;
 }
 
 function getNameKcal(menuItem) {
 
-  return menuItem.burger + ", " + menuItem.calories + " kCal";
+  return menuItem.name + ", " + menuItem.calories + " kCal";
 }
 
-let soph = new menuItem("The sophisticated burger", "None", "400", "https://www.theawesomegreen.com/wp-content/uploads/2014/09/Beet-Burger.jpg");
-document.getElementById("b1").innerHTML = getNameKcal(soph);
+let soph = new menuItem("The sophisticated burger", "A beautiful burger for all my fancy pants out there.", "It's even gluten free n shit.", "It'll keep you full but it won't make you happy.", false, "400", "https://www.theawesomegreen.com/wp-content/uploads/2014/09/Beet-Burger.jpg");
+//document.getElementById("b1").innerHTML = getNameKcal(soph);
 
-let greedy = new menuItem("The greedy burger", "Gluten", "1500", "https://static01.nyt.com/images/2019/10/23/dining/22fakemeat1/22fakemeat1-articleLarge.jpg?quality=75&auto=webp&disable=upscale");
-document.getElementById("b2").innerHTML = getNameKcal(greedy);
+let greedy = new menuItem("The greedy burger", "A thicc burger for all you protein deficient vegans.", "Will fill that big black hole of emptiness inside you and then some.", "We won't be held responsible for any jaw fractures.", true, "1500", "https://static01.nyt.com/images/2019/10/23/dining/22fakemeat1/22fakemeat1-articleLarge.jpg?quality=75&auto=webp&disable=upscale");
+//document.getElementById("b2").innerHTML = getNameKcal(greedy);
 
-let dirty = new menuItem("The dirty burger", "Gluten", "3000", "https://www.theawesomegreen.com/wp-content/uploads/2016/01/Vegan-Burger.jpg");
-document.getElementById("b3").innerHTML = getNameKcal(dirty);
+let dirty = new menuItem("The dirty burger", "A filthy burger with too much mayo for when you're feeling nasty.", "Seriously this one is gross, don't eat it if you're having a good day.", "At least it's low on cholesterol.", true, "3000", "https://www.theawesomegreen.com/wp-content/uploads/2016/01/Vegan-Burger.jpg");
+//document.getElementById("b3").innerHTML = getNameKcal(dirty);
 
-let randomBurger1 = new menuItem("Random burger 1", "None", "1000", "picture missing");
-document.getElementById("b4").innerHTML = getNameKcal(randomBurger1);
+let randomBurger1 = new menuItem("Random burger 1", "d1", "d2", "d3", false, "1000", "https://www.pinclipart.com/picdir/big/13-132538_burger-svg-png-icon-free-download-545640-clip.png");
+//document.getElementById("b4").innerHTML = getNameKcal(randomBurger1);
 
-let randomBurger2 = new menuItem("Random burger 2", "None", "1000", "picture missing");
-document.getElementById("b5").innerHTML = getNameKcal(randomBurger2);
+let randomBurger2 = new menuItem("Random burger 2", "d1", "d2", "d3", false, "1000", "https://www.pinclipart.com/picdir/big/13-132538_burger-svg-png-icon-free-download-545640-clip.png");
+//document.getElementById("b5").innerHTML = getNameKcal(randomBurger2);
 
 menu = [soph, greedy, dirty, randomBurger1, randomBurger2];
 
-let element = document.getElementById("menuID");
-for (let item of menu) {
-	let listLine = document.createElement("li");
-  let listText = document.createTextNode(item.burger);
-  listLine.appendChild(listText);
-  element.appendChild(listLine);
+let burgerElem = document.getElementsByClassName("burger");
+console.log(burgerElem);
+for (let i = 0; i < burgerElem.length; i++) {
+
+	let title = document.createElement("h3");
+  let titleText = document.createTextNode(menu[i].name);
+  let img = document.createElement("img");
+  img.src = menu[i].path;
+  img.width = 400;
+
+  let list = document.createElement("ul");
+  let desc1 = document.createElement("li");
+  let d1 = document.createTextNode(menu[i].d1);
+  let desc2 = document.createElement("li");
+  let d2 = document.createTextNode(menu[i].d2);
+  let desc3 = document.createElement("li");
+  let d3 = document.createTextNode(menu[i].d3);
+
+  let gluten = document.createElement("li");
+  let glut;
+
+
+  if (menu[i].gluten == true) {
+    glut = document.createTextNode("Allergies: Gluten.");
+  }
+  if (menu[i].gluten == false){
+    glut = document.createTextNode("Allergies: None.");
+  }
+
+  let calories = document.createElement("li");
+  let cal = document.createTextNode(menu[i].calories + " kCal.");
+
+  burgerElem[i].appendChild(img);
+  burgerElem[i].appendChild(title);
+  burgerElem[i].appendChild(list);
+
+  title.appendChild(titleText);
+
+  list.appendChild(desc1);
+  list.appendChild(desc2);
+  list.appendChild(desc3);
+  list.appendChild(gluten);
+  list.appendChild(calories);
+
+  desc1.appendChild(d1);
+  desc2.appendChild(d2);
+  desc3.appendChild(d3);
+  gluten.appendChild(glut);
+  calories.appendChild(cal);
+
+
 }
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,11 +1,13 @@
+/*
 const vm = new Vue({
   el: '#burgers',
   data: {
     menu: [soph, greedy, dirty, randomBurger1, randomBurger2]
-    /*burger1: getNameKcal(soph),
+    burger1: getNameKcal(soph),
     burger2: getNameKcal(greedy),
     burger3: getNameKcal(dirty),
     burger4: getNameKcal(randomBurger1),
-    burger5: getNameKcal(randomBurger2)*/
+    burger5: getNameKcal(randomBurger2)
   }
 })
+*/
