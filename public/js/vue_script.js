eb5c1690eb89e37ed4d6e7cc1fd8df2e06be202d
Miranda Kiss-Deáki <miranda.kissdeaki@gmail.com>
2020-04-03 15:40:30 +0200

    weird error

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -15,6 +15,7 @@ const vm = new Vue({
     radioOption: "female",
     array: "",
     orders: {},
+    order: {details: {x: 0, y: 0},},
     food,
     checkedBurgers: [],
   },
@@ -23,42 +24,22 @@ const vm = new Vue({
       this.array = this.nameInput + this.emailInput /*+ this.streetInput + this.numberInput */+ this.paymentInput + this.radioOption + this.checkedBurgers
     },
     created: function() {
-      /* When the page is loaded, get the current orders stored on the server.
-       * (the server's code is in app.js) */
       socket.on('initialize', function(data) {
         this.orders = data.orders;
       }.bind(this));
-  
-      /* Whenever an addOrder is emitted by a client (every open map.html is
-       * a client), the server responds with a currentQueue message (this is
-       * defined in app.js). The message's data payload is the entire updated
-       * order object. Here we define what the client should do with it.
-       * Spoiler: We replace the current local order object with the new one. */
       socket.on('currentQueue', function(data) {
         this.orders = data.orders;
       }.bind(this));
     },
     methods: {
       getNext: function() {
-        /* This function returns the next available key (order number) in
-         * the orders object, it works under the assumptions that all keys
-         * are integers. */
         let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
           return Math.max(last, next);
         }, 0);
         return lastOrder + 1;
       },
       addOrder: function(event) {
-        /* When you click in the map, a click event object is sent as parameter
-         * to the function designated in v-on:click (i.e. this one).
-         * The click event object contains among other things different
-         * coordinates that we need when calculating where in the map the click
-         * actually happened. */
-        let offset = {
-          x: event.currentTarget.getBoundingClientRect().left,
-          y: event.currentTarget.getBoundingClientRect().top,
-        };
-        console.log(this);
+        
         socket.emit('addOrder', {
           orderId: this.getNext(),
           details: {
@@ -68,6 +49,14 @@ const vm = new Vue({
           orderItems: ['Beans', 'Curry'],
         });
       },
+      displayOrder: function(event) {
+        let offset = {
+          x: event.currentTarget.getBoundingClientRect().left,
+          y: event.currentTarget.getBoundingClientRect().top,
+        };
+        this.order.details.x = event.clientX - 10 - offset.x,
+        this.order.details.y = event.clientY - 10 - offset.y,    
+      }
     
-  },
+    },
 });
